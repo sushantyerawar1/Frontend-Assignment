c9a57dbf3be2c5addfe78bdb0148637481cbadf7
sushantyerawar1 <sushantdyerawar@gmail.com>
2024-03-08 16:08:45 +0530

    Removed all commented lines

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Metrics from './pages/metrics/Metrics';
 import Logs from './pages/logs/Logs';
-import ChartComponent from './pages/chartjs/ChartJs';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Metrics />} exact />
         <Route path="/logs" element={<Logs />} />
-        <Route path="/chart" element={<ChartComponent />} />
       </Routes>
     </BrowserRouter>
   );
